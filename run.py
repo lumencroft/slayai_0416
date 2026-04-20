e280730d24f24bf524eb3f80cb7bc05ff2f6de8e
returndeneb <lumencroft@gmail.com>
2026-04-20 21:13:34 +0900

    줄이는중 2

--- run.py
+++ run.py
@@ -28,40 +28,40 @@ def main():
         if not action_payload or action_payload.get("action") == "wait":
             continue
 
-        success = client.send_action(action_payload)
-        if success:
-            act_type = action_payload.get("action")
-            timeout_ticks = 0
-            
-            while True:
-                check_state = client.get_state()
-                if check_state is None:
-                    continue
-                    
-                timeout_ticks += 1
-                if timeout_ticks > 150:
-                    combat_ai.action_queue.clear()
-                    break
+        client.send_action(action_payload)
+        
+        act_type = action_payload.get("action")
+        timeout_ticks = 0
+        
+        while True:
+            check_state = client.get_state()
+            if check_state is None:
+                continue
                 
-                if act_type == "play_card":
-                    old_hand = raw_state.get("player", {}).get("hand", [])
-                    new_hand = check_state.get("player", {}).get("hand", [])
-                    old_energy = raw_state.get("player", {}).get("energy", 0)
-                    new_energy = check_state.get("player", {}).get("energy", 0)
-                    
-                    if old_hand != new_hand or old_energy != new_energy:
-                        break
-                elif act_type == "end_turn":
-                    if not check_state.get("battle", {}).get("is_play_phase", False):
-                        break
-                elif act_type == "restart_combat":
-                    if check_state.get("state_type") in ["monster", "elite", "boss"] and check_state.get("battle", {}).get("is_play_phase", False):
-                        break
-                else:
-                    if check_state.get("state_type") != state_type:
-                        break
-                    if check_state.get("screen_type") != raw_state.get("screen_type"):
-                        break
+            timeout_ticks += 1
+            if timeout_ticks > 150:
+                combat_ai.action_queue.clear()
+                break
+            
+            if act_type == "play_card":
+                old_hand = raw_state.get("player", {}).get("hand", [])
+                new_hand = check_state.get("player", {}).get("hand", [])
+                old_energy = raw_state.get("player", {}).get("energy", 0)
+                new_energy = check_state.get("player", {}).get("energy", 0)
+                
+                if old_hand != new_hand or old_energy != new_energy:
+                    break
+            elif act_type == "end_turn":
+                if not check_state.get("battle", {}).get("is_play_phase", False):
+                    break
+            elif act_type == "restart_combat":
+                if check_state.get("state_type") in ["monster", "elite", "boss"] and check_state.get("battle", {}).get("is_play_phase", False):
+                    break
+            else:
+                if check_state.get("state_type") != state_type:
+                    break
+                if check_state.get("screen_type") != raw_state.get("screen_type"):
+                    break
         
 if __name__ == "__main__":
     main()
